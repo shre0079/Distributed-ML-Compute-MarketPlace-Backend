4a005d71bda3338505ddb4f88672100792cef813
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-02-06 23:38:48 +0530

    added register & ping facility

--- src/main/java/com/dcm/backend/demo/controller/MockController.java
+++ src/main/java/com/dcm/backend/demo/controller/MockController.java
@@ -1,4 +1,26 @@
 package com.dcm.backend.demo.controller;
 
+import com.dcm.backend.demo.dto.WorkerInfo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
 public class MockController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Hello Agent";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody WorkerInfo worker) {
+
+        System.out.println("Worker registered: " + worker.workerId +
+                " | CPU: " + worker.cpuCores +
+                " | RAM: " + worker.memoryMB +
+                " | OS: " + worker.os);
+
+        return "ok";
+    }
 }
