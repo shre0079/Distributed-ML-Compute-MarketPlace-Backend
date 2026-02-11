635868276c7847ab06a8279e323780318a151261
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-02-11 23:44:37 +0530

    Enable Scheduling

--- src/main/java/com/dcm/backend/demo/DemoApplication.java
+++ src/main/java/com/dcm/backend/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.dcm.backend.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
