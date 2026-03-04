203bea3b7af30cafe1a999c4d097752b3ace0358
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-03-05 00:54:38 +0530

    job requirements added

--- src/main/java/com/dcm/backend/demo/dto/JobCreateRequest.java
+++ src/main/java/com/dcm/backend/demo/dto/JobCreateRequest.java
@@ -3,4 +3,8 @@ package com.dcm.backend.demo.dto;
 public class JobCreateRequest {
     public String dockerImage;
     public String fileUrl;
+
+    public int requiredCpu;
+    public int requiredMemoryMB;
+    public boolean gpuRequired;
 }
