ff49a34eff53436d6ae8942f8d09401a66c5494e
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-06-05 16:58:44 +0530

    "transactions" table defn

--- src/main/java/com/dcm/backend/demo/dto/Transaction.java
+++ src/main/java/com/dcm/backend/demo/dto/Transaction.java
@@ -1,27 +1,21 @@
 package com.dcm.backend.demo.dto;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.math.BigDecimal;
 
-    @Entity
-    public class Transaction {
+@Entity
+@Table(name = "transactions")
+public class Transaction {
 
-        @Id
-        public String transactionId;
-
-        public String userId;
-
-        public String workerId;
-
-        public String jobId;
-
-        public String type; // DEPOSIT / JOB_COST / WORKER_PAYOUT
-
-        @Column(precision = 12, scale = 8)
-        public BigDecimal amount;
-
-        public long timestamp;
-    }
+    @Id
+    public String transactionId;
+    public String userId;
+    public String workerId;
+    public String jobId;
+    public String type;
+    public BigDecimal amount;
+    public long timestamp;
+}
