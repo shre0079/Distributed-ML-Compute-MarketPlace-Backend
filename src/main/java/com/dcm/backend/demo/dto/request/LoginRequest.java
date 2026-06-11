f927e704e53f7438da0160637d39c04d0cc75565
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-06-12 02:53:07 +0530

    add: validation constraints for password

--- src/main/java/com/dcm/backend/demo/dto/request/LoginRequest.java
+++ src/main/java/com/dcm/backend/demo/dto/request/LoginRequest.java
@@ -1,6 +1,15 @@
 package com.dcm.backend.demo.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public class LoginRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     public String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Must be alphanumeric (letters and numbers only)")
     public String password;
 }
