8ac800d5267ccb98a882dce3791cc641708785d6
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-06-11 17:40:46 +0530

    exception handling: insufficient balance

---
+++ src/main/java/com/dcm/backend/demo/exception/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package com.dcm.backend.demo.exception;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
